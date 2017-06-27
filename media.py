3ba6958ad42987f4028faebc26680350d95797e7
Tripp Green <vertusmedia@gmail.com>
2017-06-26 23:56:07 -0400

    PEP8 Fix 2

--- media.py
+++ media.py
@@ -3,16 +3,25 @@
 """
 Created on Fri Jun 23 12:55:17 2017
 
-Class for a Movie containing youtube url, title, poster url
+Class for a Movie containing
+youtube url, title, poster url
 
-@author: Tripp
+@author: Tripp Green
 """
 
+
 class Movie():
-    
+    """Creates a movie object.
+
+    Args:
+    title: Movie title
+    trailer_youtube_url: URL to film's trailer
+    poster_image_url: URL to film's poster
+
+    Returns: Void"""
+
     def __init__(self, title, trailer_youtube_url, poster_image_url):
-        
+
         self.title = title
         self.trailer_youtube_url = trailer_youtube_url
         self.poster_image_url = poster_image_url
-        
